3e09604ed41f317dcba6a2c2396371cbdadd254b
Randula Mawaththa <randula98@gmail.com>
2024-07-17 15:22:13 +0530

    typo fixed
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- components/organisms/DashboardHeader.tsx
+++ components/organisms/DashboardHeader.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const DashboardHeaher = () => {
+const DashboardHeader = () => {
     return (
         <View>
-            <Text>DashboardHeaher</Text>
+            <Text>DashboardHeader</Text>
         </View>
     )
 }
 
-export default DashboardHeaher
+export default DashboardHeader
