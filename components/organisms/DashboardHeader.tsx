0842373e9d77ba2d7874e32fb3a05bebf1d42c94
Randula Mawaththa <randula98@gmail.com>
2024-07-17 17:35:22 +0530

    Header Component Content Added
    Signed-off-by: Randula Mawaththa <randula98@gmail.com>

--- components/organisms/DashboardHeader.tsx
+++ components/organisms/DashboardHeader.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const DashboardHeader = () => {
+type DashboardHeaderProps = {
+    title: string;
+    subtitle?: string;
+};
+
+const DashboardHeader = ({ title, subtitle }: DashboardHeaderProps) => {
+    // TODO: Implement Drawer Navigation
+    const handleMenuPress = () => {
+        Alert.alert('TODO ! ', 'Implement Drawer Navigation');
+    };
+
     return (
         <View>
-            <Text>DashboardHeader</Text>
+            <View className='flex-row items-center justify-between px-4 py-2'>
+                <View>
+                    <Text className='text-2xl font-bold text-audi-purple'>{title}</Text>
+                </View>
+                <TouchableOpacity className="p-2" onPress={handleMenuPress}>
+                    <Ionicons name="menu" size={30} color="#4B0082" />
+                </TouchableOpacity>
+            </View>
+            <View className='px-4'>
+                {subtitle && (
+                    <Text className='text-base text-gray-700'>{subtitle}</Text>
+                )}
+            </View>
         </View>
-    )
-}
+    );
+};
 
-export default DashboardHeader
+export default DashboardHeader;
