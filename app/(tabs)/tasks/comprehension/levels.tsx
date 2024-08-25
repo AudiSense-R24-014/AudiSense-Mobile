2c3b194b874d8b1843fc74933d0538e4f3271bd7
SuDelk <sudul.fernando@gmail.com>
2024-08-25 21:17:40 +0530

    prepared levls for stages

---
+++ app/(tabs)/tasks/comprehension/levels.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const Levels = () => {
+  return (
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 items-center p-4 justify-between">
+        <Text>Levels</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default Levels;
