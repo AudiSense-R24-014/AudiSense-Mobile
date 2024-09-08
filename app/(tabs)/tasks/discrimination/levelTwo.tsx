187c62501e942e8a2d53f7be72dce1725aa3cc4c
GRieZzLEO <chamodhleo1@gmail.com>
2024-09-09 02:22:16 +0530

    Added Discrimination Voice

--- app/(tabs)/tasks/discrimination/levelTwo.tsx
+++ app/(tabs)/tasks/discrimination/levelTwo.tsx
@@ -5,6 +5,7 @@ import DiscriminationTaskService from '@/services/DiscriminationTask.service';
 import QuestionButton from '@/components/molecules/QuestionButton';
 import AnswerButton from '@/components/molecules/AnswerButton';
 import SpeechInput from '@/components/molecules/SpeechInput';
+import { storage, ref, uploadBytes, getDownloadURL } from "@/firebaseConfig";
 
 const DiscriminationLevel2 = () => {
     const [isPlayingFour, setIsPlayingFour] = useState(false);
@@ -14,12 +15,13 @@ const DiscriminationLevel2 = () => {
 
     const [firstWord, setFirstWord] = useState("");
     const [secondWord, setSecondWord] = useState("");
-  
+    const [status, setStatus] = useState("");
+
     useEffect(() => {
-      DiscriminationTaskService.getDiscriminationTaskById("66db2163230c2790b39a8df3").then((data) => {
-        setFirstWord(data?.word1);
-        setSecondWord(data?.word2);
-      });
+        DiscriminationTaskService.getDiscriminationTaskById("66db2163230c2790b39a8df3").then((data) => {
+            setFirstWord(data?.word1);
+            setSecondWord(data?.word2);
+        });
     }, []);
 
     async function startRecording() {
@@ -51,7 +53,7 @@ const DiscriminationLevel2 = () => {
         setRecordings(allRecordings);
     }
 
-    function getDurationFormatted(milliseconds:any) {
+    function getDurationFormatted(milliseconds: any) {
         const minutes = milliseconds / 1000 / 60;
         const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
         return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`;
@@ -71,9 +73,54 @@ const DiscriminationLevel2 = () => {
     function clearRecordings() {
         setRecordings([]);
     }
-    const capitalizeFirstLetter = (word:string) => {
+    const capitalizeFirstLetter = (word: string) => {
         return word.charAt(0).toUpperCase() + word.slice(1);
-      };
+    };
+
+    const [lockedRecordings, setLockedRecordings] = useState<any[]>([]);
+
+    const submit = async () => {
+        try {
+            const urls = [];
+
+            // Iterate over locked recordings and upload each to Firebase Storage
+            for (let i = 0; i < lockedRecordings.length; i++) {
+                const recording = lockedRecordings[i];
+
+                if (recording?.file) {
+                    const response = await fetch(recording.file); // Get the file from the URI
+                    const blob = await response.blob(); // Convert the file to a Blob
+                    const storageRef = ref(storage, `recordings/${Date.now()}_${i}.mp3`);
+                    // Upload the blob to Firebase Storage
+                    await uploadBytes(storageRef, blob);
+                    const downloadURL = await getDownloadURL(storageRef);
+                    urls[i] = downloadURL;
+                }
+            }
+            saveProgress();
+            console.log("Uploaded recording URLs:", urls);
+        } catch (error) {
+            console.error("Error uploading recordings:", error);
+        }
+    };
+
+    const saveProgress = async () => {
+        const feedback = {
+            word1: firstWord,
+            word2: secondWord,
+            status: "completed",
+            level: "2",
+            providedAnswers: lockedRecordings,
+            patient: "Leo",
+        };
+        DiscriminationTaskService.saveDiscriminationActvityResponse(feedback)
+            .then((data) => {
+                console.log("Rhyming Words saved Successfully: ", data);
+            })
+            .catch((err) => {
+                console.error("Error saving: ", err);
+            });
+    };
 
     return (
         <View className="flex-1 bg-white items-center">
@@ -84,58 +131,41 @@ const DiscriminationLevel2 = () => {
 
             <View className="flex-row justify-around w-full mb-20">
                 <View className="items-center w-2/5">
-                    {/* <TouchableOpacity
-                        className="bg-purple-100 p-5 rounded-lg items-center"
-                        disabled={isPlayingFour}
-                    >
-                        <View className="flex-row items-center w-4/5 h-1 bg-gray-300 rounded">
-                            <Image className="w-5 h-5 mr-2" />
-                            <View className="flex-1 h-1 bg-purple-700 rounded" />
-                        </View>
-                    </TouchableOpacity> */}
                     <View>
-                        <QuestionButton text={firstWord}/>
+                        <AnswerButton text={firstWord} character={''} />
                     </View>
                     <Text className="text-lg font-bold text-purple-700 mt-2">{capitalizeFirstLetter(firstWord)}</Text>
                 </View>
                 <View className="items-center w-2/5">
-                    {/* <TouchableOpacity
-                        className="bg-purple-100 p-5 rounded-lg items-center"
-                        disabled={isPlayingRoar}
-                    >
-                        <View className="flex-row items-center w-4/5 h-1 bg-gray-300 rounded">
-                            <Image className="w-5 h-5 mr-2" />
-                            <View className="flex-1 h-1 bg-purple-700 rounded" />
-                        </View>
-                    </TouchableOpacity> */}
                     <View>
-                        <AnswerButton text={secondWord} character={''}/>
+                        <AnswerButton text={secondWord} character={''} />
                     </View>
                     <Text className="text-lg font-bold text-purple-700 mt-2">{capitalizeFirstLetter(secondWord)}</Text>
                 </View>
             </View>
-            <View>
-                <SpeechInput lockRecording={function (recording: any): void {
-                    throw new Error('Function not implemented.');
-                } } recordedAudio={undefined}/>
+            <View className="flex-row justify-between">
+                <View className="flex-1 mr-2">
+                    <SpeechInput
+                        lockRecording={function (recording: any): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        recordedAudio={undefined}
+                    />
+                </View>
+                <View className="flex-1">
+                    <SpeechInput
+                        lockRecording={function (recording: any): void {
+                            throw new Error('Function not implemented.');
+                        }}
+                        recordedAudio={undefined}
+                    />
+                </View>
             </View>
-
-            <View className="flex-row justify-center mb-20 space-x-32">
-                <TouchableOpacity
-                    className="bg-purple-700 p-5 rounded-full"
-                    onPress={recordingAudio ? stopRecording : startRecording}
-                >
-                    <Text style={{ color: 'white', fontSize: 16 }}>{recordingAudio ? 'Stop' : 'Start'}</Text>
+            <View className='mt-8'>
+                <TouchableOpacity className="bg-purple-800 p-2 rounded-lg w-3/4 mx-auto" onPress={submit}>
+                    <Text className="text-center text-base text-white font-bold">Check</Text>
                 </TouchableOpacity>
             </View>
-
-            {getRecordingLines()}
-
-            {recordings.length > 0 && (
-                <TouchableOpacity onPress={clearRecordings} className="bg-red-500 p-5 rounded-lg">
-                    <Text style={{ color: 'white', fontSize: 16 }}>Clear Recordings</Text>
-                </TouchableOpacity>
-            )}
         </View>
     );
 };
--- services/DiscriminationTask.service.tsx
+++ services/DiscriminationTask.service.tsx
@@ -10,6 +10,32 @@ const getDiscriminationTaskById = async (id : any) => {
   const data = await response.json();
   return data;
 };
+
+const saveDiscrimantionTaskResponse = async (id : any, body : any) => {
+  const response = await fetch(URL.DISCRIMINATION_TASK_BY_ID(id), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+}
+
+const saveDiscriminationActvityResponse = async (req : any) => {
+  
+  const response = await fetch(URL.DISCRIMINATION_ACTIVITY, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+  const data = await response.json();
+  return data;
+}
 export default{
     getDiscriminationTaskById,
+    saveDiscriminationActvityResponse,
 }
